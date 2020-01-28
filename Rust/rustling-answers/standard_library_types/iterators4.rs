4f5e582b727cfc8a426d8a3e3fe00dec4a516ecc
jiazhu <garfield@bupt.edu.cn>
2020-01-29 00:01:30 +0800

    Resolve standard library types

---
+++ Rust/rustling-answers/standard_library_types/iterators4.rs
@@ -0,0 +1,26 @@
+// iterators4.rs
+
+// DONE！ 🥳
+
+pub fn factorial(num: u64) -> u64 {
+    (1..=num).fold(1, |acc, x| acc * x)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn factorial_of_1() {
+        assert_eq!(1, factorial(1));
+    }
+    #[test]
+    fn factorial_of_2() {
+        assert_eq!(2, factorial(2));
+    }
+
+    #[test]
+    fn factorial_of_4() {
+        assert_eq!(24, factorial(4));
+    }
+}
