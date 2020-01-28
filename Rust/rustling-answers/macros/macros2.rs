e37406bcb09f1fec81d05eb56a755c585f452629
jiazhu <garfield@bupt.edu.cn>
2020-01-28 14:09:45 +0800

    Resolve macros and move semantics

---
+++ Rust/rustling-answers/macros/macros2.rs
@@ -0,0 +1,14 @@
+// macros2.rs
+// Make me compile! Execute `rustlings hint macros2` for hints :)
+
+// DONE！ 🥳
+
+macro_rules! my_macro {
+    () => {
+        println!("Check out my macro!");
+    };
+}
+
+fn main() {
+    my_macro!();
+}
---
+++ Rust/rustling-answers/test4.rs
@@ -0,0 +1,19 @@
+// test4.rs
+// This test covers the sections:
+// - Modules
+// - Macros
+
+// Write a macro that passes the test! No hints this time, you can do it!
+macro_rules! my_macro {
+    ($val:expr) => {
+        "Hello {}".to_owned() + $val
+    }
+}
+
+// DONE！ 🥳
+
+fn main() {
+    if my_macro!("world!") != "Hello world!" {
+        panic!("Oh no! Wrong output!");
+    }
+}
