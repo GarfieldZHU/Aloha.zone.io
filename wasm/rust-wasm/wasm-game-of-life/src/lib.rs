18d965b249df99c159c738511f3300785bb8c8c3
jiazhu <garfield@bupt.edu.cn>
2020-02-07 17:48:36 +0800

    Add text rendering and interface to JS

--- wasm/rust-wasm/wasm-game-of-life/src/lib.rs
+++ wasm/rust-wasm/wasm-game-of-life/src/lib.rs
@@ -1,6 +1,7 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use std::fmt;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -26,6 +27,22 @@ pub struct Universe {
     cells: Vec<Cell>,
 }
 
+// Render the universe state as text
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+
+        Ok(())
+    }
+}
+
+// Implement private methods of Universe for WASM side
 impl Universe {
     // Given the postion of cell, get the index of it in Universe
     fn get_index(&self, row: u32, column: u32) -> usize {
@@ -36,7 +53,7 @@ impl Universe {
     fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
         let mut count = 0;
         // Loop all 8 neighbor cells by exlucding itself from the 3*3 matrix
-        // Avoid unsigned integer underflow, use `self.height - 1` rather than `-1`
+        // Avoid unsigned integer underflow, use `self.height - 1` with modulo rather than `-1`
         for delta_row in [self.height - 1, 0, 1].iter().cloned() {  
             for delta_col in [self.width - 1, 0, 1].iter().cloned() {
                 if delta_row == 0 && delta_col == 0 {
@@ -50,6 +67,71 @@ impl Universe {
             }
         }
         count
+    }
+}
+
+// Public methods, exported to JavaScript
+#[wasm_bindgen]
+impl Universe {
+    // Trigger a tick, calculate the next generation of universe
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighbor_count(row, col);
+
+                let next_cell = match (cell, live_neighbors) {
+                    // Rule 1: Any live cell with fewer than two live neighbours
+                    // dies, as if caused by underpopulation.
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    // Rule 2: Any live cell with two or three live neighbours
+                    // lives on to the next generation.
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    // Rule 3: Any live cell with more than three live
+                    // neighbours dies, as if by overpopulation.
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    // Rule 4: Any dead cell with exactly three live neighbours
+                    // becomes a live cell, as if by reproduction.
+                    (Cell::Dead, 3) => Cell::Alive,
+                    // All other cells remain in the same state.
+                    (otherwise, _) => otherwise,
+                };
+
+                next[idx] = next_cell;
+            }
+        }
+
+        self.cells = next;
+    }
+
+    // Constructor. Initialize the size of universe with initial pattern
+    pub fn new() -> Universe {
+        let width = 64;
+        let height = 64;
+
+        let cells = (0..width * height)
+            .map(|i| {  // Define a rule for initial pattern here
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    // Get a formatted string 
+    pub fn render(&self) -> String {
+        self.to_string()
     }
 }
 
