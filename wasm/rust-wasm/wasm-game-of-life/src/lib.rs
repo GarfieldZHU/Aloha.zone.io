a9b7d8bc45f33f36cad08ef6715343fb88287da3
jiazhu <garfield@bupt.edu.cn>
2020-02-07 17:34:14 +0800

    Add Cell and Universe

--- wasm/rust-wasm/wasm-game-of-life/src/lib.rs
+++ wasm/rust-wasm/wasm-game-of-life/src/lib.rs
@@ -8,6 +8,51 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+// Single cell state. 
+// One byte length. As be calcualted as u8.
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+// The linear structure for 2-dimension universe
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+impl Universe {
+    // Given the postion of cell, get the index of it in Universe
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    // Given the position of cell, count how many neighbors of it are alive
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        // Loop all 8 neighbor cells by exlucding itself from the 3*3 matrix
+        // Avoid unsigned integer underflow, use `self.height - 1` rather than `-1`
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {  
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
+}
+
 #[wasm_bindgen]
 extern {
     fn alert(s: &str);
