c2abeae09316682f73050ec66c50eb391f611ef5
jiazhu <garfield@bupt.edu.cn>
2020-02-13 09:12:25 +0800

    Add UT

--- wasm/rust-wasm/wasm-game-of-life/tests/web.rs
+++ wasm/rust-wasm/wasm-game-of-life/tests/web.rs
@@ -28,3 +28,17 @@ pub fn expected_spaceship() -> Universe {
     universe.set_cells(&[(2,1), (2,3), (3,2), (3,3), (4,2)]);
     universe
 }
+
+#[wasm_bindgen_test]
+pub fn test_tick() {
+    // Let's create a smaller Universe with a small spaceship to test!
+    let mut input_universe = input_spaceship();
+
+    // This is what our spaceship should look like
+    // after one tick in our universe.
+    let expected_universe = expected_spaceship();
+
+    // Call `tick` and then see if the cells in the `Universe`s are the same.
+    input_universe.tick();
+    assert_eq!(&input_universe.get_cells(), &expected_universe.get_cells());
+}
