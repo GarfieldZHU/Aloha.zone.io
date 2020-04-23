a9d8e3222fce6edf130835ec3482bd5a25f0371f
jiazhu <garfield@bupt.edu.cn>
2020-04-23 22:41:23 +0800

    update curry practise

--- TypeScript/FP/curry.ts
+++ TypeScript/FP/curry.ts
@@ -5,45 +5,66 @@ import curry from "https://deno.land/x/denofun/lib/curry.ts";
 
 //----------------------Exercise----------------------
 
-console.log('add', map(add(2)));
+console.log('add', map(add(2)))
 
 
 // Exercise 1
+// Use partial apply to simplify:
+
+// const words = function(str) {
+//   return split(' ', str)
+// }
 //==============
 
-const words = split(' ');
+const words = split(' ')
 
 // Exercise 1a
+// Use `map` to create a new `words` method
+
+//sentences = undefined
 //==============
 
-const sentences = map(words);
+const sentences = map(words)
 
 
 // Exercise 2
+// Use partial apply to simplify:
+
+// function filterQs (xs: string) {
+//   return filter(function(x){ return match(/q/i, x);  }, xs)
+// }
+
 //==============
 
-const filterQs = filter(match(/q/i));
+const filterQs = filter(match(/q/i))
 
 
 // Exercise 3
-//==============
 // Use the helper function _keepHighest to refactor max
 
-const _keepHighest = function(x, y) { return x >= y ? x : y; }; // <- leave be
+const _keepHighest = function(x: number, y: number) { return x >= y ? x : y } // <- leave be
 
-const max = reduce(_keepHighest, -Infinity);
+// function max(xs: string) {
+//   return reduce(function(acc, x){
+//     return _keepHighest(acc, x)
+//   }, -Infinity, xs)
+// }
+
+//==============
+
+const max = reduce(_keepHighest, -Infinity)
 
 
 // Bonus 1:
-// ============
 // wrap array's slice to be functional and curried.
 // //[1,2,3].slice(0, 2)
-const slice = _.curry(function(start, end, xs){ return xs.slice(start, end); });
+// ============
+const slice = curry(function(start: number, end: number, xs: string){ return xs.slice(start, end); })
 
 
 // Bonus 2:
-// ============
 // use slice to define a function "take" that takes n elements. make it curried
-const take = slice(0);
+// ============
+const take = slice(0)
 
 
