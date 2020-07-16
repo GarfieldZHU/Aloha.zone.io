24942b0d49e8843e7e226eba8f52b90cb5d9bafd
jiazhu <garfield@bupt.edu.cn>
2020-07-17 00:44:50 +0800

    migrate: remove build targets

--- Graphics/RayTracing/target/debug/deps/raytracing-6469b9fc40aaa5b1.d
+++
@@ -1,5 +0,0 @@
-/Users/Garfield/Code/AlohaNotes/Graphics/RayTracing/target/debug/deps/raytracing-6469b9fc40aaa5b1: src/main.rs
-
-/Users/Garfield/Code/AlohaNotes/Graphics/RayTracing/target/debug/deps/raytracing-6469b9fc40aaa5b1.d: src/main.rs
-
-src/main.rs:
--- Graphics/RayTracing/target/debug/deps/raytracing-90dae112b41dfb36.d
+++
@@ -1,5 +0,0 @@
-/Users/Garfield/Code/AlohaNotes/Graphics/RayTracing/target/debug/deps/raytracing-90dae112b41dfb36: src/main.rs
-
-/Users/Garfield/Code/AlohaNotes/Graphics/RayTracing/target/debug/deps/raytracing-90dae112b41dfb36.d: src/main.rs
-
-src/main.rs:
